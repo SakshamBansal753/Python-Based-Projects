62735f866fcd17804ae8202cca852fc76dd41bda
Saksham bansal <kanhabansal563@gmail.com>
2025-03-23 15:46:25 +0530

    Create data.py

---
+++ Game/data.py
@@ -0,0 +1,42 @@
+
+
+easy_villans={
+    "Zombie":{
+        "attacks":[["Memory Eater",200],["brain eater",200],["blood bath",250],["Rage",200]]
+    },
+    "Vampire":{
+
+        "attacks":[["Blooddrain",200],["redbite",150],["Bloody moonfall",210],["Delusiion",200]]
+
+    },
+    "Wolfman":{
+
+        "attacks":[["Blood bite",210],["Howling crash",200],["Fury Swipe",210],["Howling bite",250]]
+    }
+}
+user_attacks=[["1)Blood rise",200],["2)Riffle Assault",170],["3)Holy Cross Throw",210],["4)Grenade Burst ",200]]
+
+medium_villains={
+    "Dracula":{
+        "attacks":[["Bat Swarm",210],["Dracula bite",210],["Dark Magic Blast",210],["Shadow Drain",190]]
+
+    },
+    "MagmaGolem":{
+        "attacks":[["Titan Punch",200],["Molten Rock Throw ",210],["Eruption Wave",170],["Heat Pulse",210]]
+    },
+
+    "Lavarock": {
+        "attacks": [["Titan Punch", 200], ["Molten Rock Throw ", 210], ["Eruption Wave", 170], ["Heat Pulse", 210]]
+    }
+}
+hard_villans={
+    "Witch":{
+        "attacks":[["Magic Blast",210],["Magic Ray",200],["Toxic Potion Throw",190],["MAgic Hypnosis Blast",200]]
+    },
+    "Thanos":{
+        "attacks":[["Titan Punch ",200],["Power Stone strike ",210],["Infinity Punch",190],["Double Blade Scythe ",200 ]]
+    },
+    "Demon":{
+        "attacks":[["Blood Bath",190],["Demon strike ",210],["Shadow Fire ",200],["Demon Control ",200 ]]
+    }
+}
