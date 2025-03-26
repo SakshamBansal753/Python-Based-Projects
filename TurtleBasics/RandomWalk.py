bd5c8448e38b98160bfdc0658e77561d9ae827d3
Saksham bansal <kanhabansal563@gmail.com>
2025-03-27 00:29:53 +0530

    Update RandomWalk.py

--- TurtleBasics/RandomWalk.py
+++ TurtleBasics/RandomWalk.py
@@ -6,14 +6,15 @@ timmy_the_turtle=Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("blue","green")
 timmy_the_turtle.position()
-
+colours=["CornflowerBlue","Black","red","green","yellow","pink",'SeaGreen']
 directions=[0,90,180,270]
+
 
 is_true=True
 while is_true:
     timmy_the_turtle.forward(30)
     timmy_the_turtle.setheading(random.choice(directions))
-
+    timmy_the_turtle.color(random.choice(colours))
 
 screen=Screen()
 screen.exitonclick()
