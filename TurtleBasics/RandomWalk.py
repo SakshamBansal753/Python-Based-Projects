891ded96c3845e2de8dbda507470d1564804b70a
Saksham bansal <kanhabansal563@gmail.com>
2025-03-27 00:38:02 +0530

    Update RandomWalk.py

--- TurtleBasics/RandomWalk.py
+++ TurtleBasics/RandomWalk.py
@@ -5,8 +5,17 @@ from turtle import Turtle, Screen
 timmy_the_turtle=Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("blue","green")
+turtle.colormode(255)
+def random_color():
+    r=random.randint(0,255)
+    b = random.randint(0, 255)
+    g = random.randint(0, 255)
+
+    random_col=(r,b,g)
+    return random_col
+
 timmy_the_turtle.position()
-colours=["CornflowerBlue","Black","red","green","yellow","pink",'SeaGreen']
+
 directions=[0,90,180,270]
 timmy_the_turtle.pensize(15)
 timmy_the_turtle.speed(0)
@@ -15,7 +24,8 @@ is_true=True
 while is_true:
     timmy_the_turtle.forward(30)
     timmy_the_turtle.setheading(random.choice(directions))
-    timmy_the_turtle.color(random.choice(colours))
+    timmy_the_turtle.color(random_color())
+
 
 screen=Screen()
 screen.exitonclick()
