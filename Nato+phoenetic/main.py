9be9715ba044b32f9509b318ade6682a7770fd85
Saksham bansal <kanhabansal563@gmail.com>
2025-04-30 22:39:23 +0530

    Update main.py

--- Nato+phoenetic/main.py
+++ Nato+phoenetic/main.py
@@ -2,7 +2,18 @@ import pandas
 
 data=pandas.read_csv("./nato_phonetic_alphabet.csv")
 phonetic_dict={row.letter:row.code for (index,row) in data.iterrows()}
-word=input("Enter Your name").upper()
 
-name=[phonetic_dict[letter] for letter in word]
-print(name)
+is_ok=True
+while  is_ok:
+    word = input("Enter Your name").upper()
+    try:
+        name = [phonetic_dict[letter] for letter in word]
+
+    except KeyError:
+        print("Sorry Enter words only")
+        is_ok=True
+    else:
+        print(name)
+        is_ok = False
+
+
