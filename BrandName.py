a4f6e116c5b5cf02412d11ff990cedfb933551d8
Saksham bansal <kanhabansal563@gmail.com>
2025-05-12 14:51:41 +0530

    Delete BrandName.py

--- BrandName.py
+++
@@ -1,4 +0,0 @@
-print("Welcome To Brand Name Generator")
-CityName=input("Enter The City You Grew Up\n")
-PetName=input("Enter the Name Of Your Pet:\n")
-print("Your Brand Name is   "+CityName+" "+PetName)
