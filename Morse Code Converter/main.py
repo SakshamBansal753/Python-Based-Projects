ef5abb4889f9e2dad2d796a5b10ceb12f92242b9
Saksham bansal <kanhabansal563@gmail.com>
2025-08-13 00:22:09 +0530

    Create main.py

---
+++ Morse Code Converter/main.py
@@ -0,0 +1,27 @@
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+    '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.',
+    ' ': '/'
+}
+
+def convertcode(ans):
+    ans=ans.upper()
+
+    m=''
+    for c in ans:
+       if c in MORSE_CODE_DICT:
+           m+=MORSE_CODE_DICT[c]
+       else:
+           m+='?'
+
+    return m.strip()
+ans=input("Enter The Text You Want To Convert Into Morse Code")
+converter=convertcode(ans)
+print(converter)
